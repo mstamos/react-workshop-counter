a0242e149c48929733b2c470f365dcae7da9401c
Miltos Stamos <miltos.stamos@theblueground.com>
2021-12-23 10:16:14 +0200

    add counter functionality and tests

--- src/App.js
+++ src/App.js
@@ -1,25 +1,104 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import PropTypes from 'prop-types';
 import './App.css';
 
-function App() {
+export function Buttons({increase, decrease}) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div>
+      <button data-testid="plus" onClick={increase}>+</button>
+      <button data-testid="minus" onClick={decrease}>-</button>
+    </div> 
+  )
 }
+
+Buttons.propTypes = {
+  increase: PropTypes.func,
+  decrease: PropTypes.func
+}
+
+function App() {
+  const [counter, setCounter] = useState(0);
+
+  const increase = () => {
+    setCounter(counter + 1);
+  }
+
+  const decrease = () => {
+    setCounter(counter - 1);
+  }
+
+  if (counter < 0) {
+    return (
+      <div className="App">
+        <div data-testid="counter-result">:-)</div>
+        <Buttons increase={increase} decrease={decrease}/>
+      </div>
+    )
+  }
+
+    return (
+      <div className="App">
+        <div data-testid="counter-result">Counter: {counter} </div>
+        <Buttons increase={increase} decrease={decrease}/>
+      </div> 
+    )
+
+}
+
+// class App extends React.Component {
+//   constructor() {
+//     super();
+//     this.state= {
+//       counter: 0
+//     }
+//   }
+
+//   increase = () => {
+//     this.setState({
+//       counter: this.state.counter + 1
+//     });
+//   }
+
+//   decrease = () => {
+//     this.setState({
+//       counter: this.state.counter - 1
+//     });
+//   }
+
+//   render() {
+//     return (
+//     <div className="App">
+//       <div>Counter: {this.state.counter}</div>
+//       <div>
+//         <button onClick={this.increase}>+</button>
+//         <button onClick={this.decrease}>-</button>
+//       </div>
+//     </div> 
+//     )
+//   }
+
+// }
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,49 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App, {Buttons} from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('App', () => {
+
+  describe('Buttons', () => {
+
+    it('renders', () => {
+      expect(render(<Buttons/>).container).toMatchSnapshot()
+    });
+
+    it('calls increase function', () => {
+      const increase = jest.fn();
+
+      render(<Buttons increase={increase}/>)
+
+      screen.getByTestId('plus').click()
+
+      expect(increase).toHaveBeenCalled();
+    })
+
+  });
+
+  it('increases the counter correctly', () => {
+    render(<App/>);
+
+    expect(screen.getByTestId('counter-result')).toHaveTextContent('Counter: 0')
+
+    screen.getByTestId('plus').click()
+
+    expect(screen.getByTestId('counter-result')).toHaveTextContent('Counter: 1')
+  })
+
+  it('decreases the counter', () => {})
+
+  it('displays the emoji when counter < 0', () => {
+    render(<App/>);
+
+    expect(screen.getByTestId('counter-result')).toHaveTextContent('Counter: 0')
+
+    screen.getByTestId('minus').click()
+
+    const actual = screen.getByTestId('counter-result');
+    const expected = ':-)';
+
+    expect(actual).toHaveTextContent(expected)
+  })
+
+})
